4e0cc4db8c7f0fef9d82ddbfac5921d452eb2f7d
Giti <giti.khavari@gmail.com>
2026-04-16 12:26:51 +0200

    improvements

---
+++ philo/routines.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routines.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkhavari <gkhavari@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 22:38:08 by gkhavari          #+#    #+#             */
+/*   Updated: 2026/04/15 22:38:26 by gkhavari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*Even Philosophers eat -> sleep -> think*/
+static void	routine_even(t_philo *philo)
+{
+	while (1)
+	{
+		pthread_mutex_lock(&philo->data->death_check);
+		if (philo->data->end_simulation == TRUE)
+		{
+			pthread_mutex_unlock(&philo->data->death_check);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->data->death_check);
+		philo_take_forks(philo);
+		philo_eat(philo);
+		philo_return_forks(philo);
+		philo_sleep(philo);
+		philo_think(philo);
+	}
+}
+
+/*Last Philosophers think -> eat -> sleep*/
+static void	routine_last(t_philo *philo)
+{
+	while (1)
+	{
+		pthread_mutex_lock(&philo->data->death_check);
+		if (philo->data->end_simulation == TRUE)
+		{
+			pthread_mutex_unlock(&philo->data->death_check);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->data->death_check);
+		philo_think(philo);
+		philo_take_forks(philo);
+		philo_eat(philo);
+		philo_return_forks(philo);
+		philo_sleep(philo);
+	}
+}
+
+/*Odd Philosophers sleep -> think -> eat*/
+static void	routine_odd(t_philo *philo)
+{
+	while (1)
+	{
+		pthread_mutex_lock(&philo->data->death_check);
+		if (philo->data->end_simulation == TRUE)
+		{
+			pthread_mutex_unlock(&philo->data->death_check);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->data->death_check);
+		philo_sleep(philo);
+		philo_think(philo);
+		philo_take_forks(philo);
+		philo_eat(philo);
+		philo_return_forks(philo);
+	}
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->id % 2 == 0)
+		routine_even(philo);
+	else if (philo->id == (int)philo->data->num_philos)
+		routin_last(philo);
+	else
+		routine_odd(philo);
+	return (NULL);
+}
